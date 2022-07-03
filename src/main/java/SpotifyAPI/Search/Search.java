3d0325143b00b81e15ca5aaa74e7ff9fb34dac79
LasseTimmermanns <Lasse.mainkonto@gmail.com>
2022-07-03 13:01:32 +0200

    search method now returns JSONArray

--- src/main/java/SpotifyAPI/Search/Search.java
+++ src/main/java/SpotifyAPI/Search/Search.java
@@ -7,14 +7,14 @@ import java.util.Map;
 
 public class Search {
 
-    public void search(String query, String bearerToken){
+    public JSONArray search(String query, String bearerToken){
         Map<String, String> params = Map.of(
                 "q", query,
                 "type", "track",
                 "market", "DE",
                 "limit", "10"
         );
-        JSONArray result = Requests.mergeItems(Requests.sendRequest("https://api.spotify.com/v1/search", params, bearerToken));
+        return Requests.mergeItems(Requests.sendRequest("https://api.spotify.com/v1/search", params, bearerToken));
     }
 
 }
